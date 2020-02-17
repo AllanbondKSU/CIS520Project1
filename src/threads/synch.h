c2c8fed6047c2a17e13cef039ba48b2ff7dfdb5d
AllanbondKSU <42525865+AllanbondKSU@users.noreply.github.com>
2020-02-16 18:56:31 -0600

    Update synch.h
    Updated synch.h

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,8 +22,8 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
-    struct list_elem elem;      /* List element for priority donation. */
-    int max_priority;    /* Max priority for priority donation. */
+    struct list_elem elem;    
+    int max_priority; 
   };
 
 void lock_init (struct lock *);
@@ -31,9 +31,7 @@ void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
-bool lock_priority_large (const struct list_elem *a,
-                          const struct list_elem *b,
-                          void *aux);
+bool lock_priority_large (const struct list_elem *a, const struct list_elem *b, void *aux);
 
 /* Condition variable. */
 struct condition 
@@ -45,9 +43,7 @@ void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
-bool cond_sema_priority_large (const struct list_elem *a,
-                               const struct list_elem *b,
-                               void *aux);
+bool cond_sema_priority_large (const struct list_elem *a,const struct list_elem *b,void *aux);
 
 /* Optimization barrier.
    The compiler will not reorder operations across an
